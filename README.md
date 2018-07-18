05136f956c769977754865e13661a451c3b91aba
Matt South <matt@mattsouth.net>
2018-07-18 10:34:29 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+A vagrant vm for working with batmanjs, which has pretty much disappeared from the internet, see
+* https://twitter.com/batmanjs
+* https://www.npmjs.com/package/batman
+
+### Dependencies:
+* VirtualBox
+* Vagrant
---
+++ provision.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y autoremove
+
+# get ready for webapp
+sudo apt-get -y install git
+sudo apt-get -y install build-essential gcc make  # Maybe needed.
+
+echo ""
+echo "Almost done.  The machine is prepared..."
+echo "Now you need to run the following commands:"
+echo "> vagrant ssh"
+echo "> curl https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash"
+echo "> source ~/.bashrc"
+echo "> nvm install 0.8.0"
+echo "> git clone https://github.com/jhnvz/batman"
+echo "> cd batman"
